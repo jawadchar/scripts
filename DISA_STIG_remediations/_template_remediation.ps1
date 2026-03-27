84f5519bc2f747248ca39c69f5d10cd0a18274f6
jawadchar <pianoandfrog@gmail.com>
2026-03-27 16:44:59 -0500

    Create _template_remediation.ps1

---
+++ DISA_STIG_remediations/_template_remediation.ps1
@@ -0,0 +1,28 @@
+<#
+.SYNOPSIS
+
+
+.NOTES
+    Author          : Jawad Charafeddine
+    LinkedIn        : linkedin.com/in/jawadchar/
+    GitHub          : github.com/jawadchar
+    Date Created    : 2024-03-01
+    Last Modified   : 2024-03-01
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : 
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+.USAGE
+    Put any usage instructions here.
+    Example syntax:
+    PS C:\> .\__remediation_template(STIG-ID-WN10-AU-000500).ps1 
+#>
+
+# YOUR CODE GOES HERE
